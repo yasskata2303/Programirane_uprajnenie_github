f074a1f954b8cd523ed157637b106cc7d3363f52
yasskata2303 <107563628+yasskata2303@users.noreply.github.com>
2023-01-26 10:00:00 +0200

    Delete quadraticroots.h

--- quadraticroots.h
+++
@@ -1,9 +0,0 @@
-#ifndef GURAD
-#define GUARD
-    struct QuadraticRootsResult{
-        long double x1, x2;
-        char norealroots;
-    };
-    struct QuadraticRootsResult findroots(long double a,long double b,long double c);
-
-#endif
